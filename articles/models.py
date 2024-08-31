0d6ab5b236117e06d34cc6a590c0330723105b9f
Olga_Noskova <perhonen_87@mail.ru>
2024-08-31 17:02:22 +0300

    Initial commit

---
+++ articles/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги статьи'
+
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tags = models.ManyToManyField(Tag, through='Scope', related_name='articles')
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        # ordering = ['-published_at']
+
+    def __str__(self):
+        return self.title
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
+
+    def clean(self):
+        if self.is_main and Scope.objects.filter(article=self.article, is_main=True).exclude(id=self.id).exists():
+            raise ValidationError('У этой статьи уже есть основной раздел.')
+
+    class Meta:
+        verbose_name = 'Тег статьи'
+        verbose_name_plural = 'Теги статей'
